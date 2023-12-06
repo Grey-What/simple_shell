3d6c97853cd317598b87f7aab50c33ee53e2fc9b
Vish <vishaylinparshotam@gmail.com>
2023-12-06 06:05:36 -0800

    constructing full pathnames

--- getpath.c
+++ getpath.c
@@ -38,9 +38,29 @@ char **getdires(char *name)
 */
 char *getpath(char *name)
 {
-	char **directories = NULL;
+	char **directories = NULL, char *pathname = NULL;
+	struct stat *temp = malloc(sizeof(struct stat ));
 
 	directories = getdires(name);
-
+	while (directories[i] != NULL)
+	{
+		pathname = malloc(sizeof(char) * (_strlen(directories[i] + _strlen(name) + 2);
+		if (pathame == NULL)
+			return (NULL);
+		_strcpy(pathname, directories[i]);
+		_strcat(pathname, "/");
+		_strcat(pathname, name);
+		if (stat(pathname, temp) == 0)
+		{
+			free_list(directories);
+			free(temp);
+			return (pathname);
+		}
+		free(pathname);
+		pathname = NULL;i
+		i++;
+	}
+	free_list(directories);
+	free(temp);
+	return (NULL);
 }
-

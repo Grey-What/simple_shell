7c25c0b62d05d3135be9fa328ca39a34d317bc9d
Grey-What <greywhat2002@gmail.com>
2023-12-08 04:00:06 -0800

    updated function prototypes

--- main.h
+++ main.h
@@ -12,11 +12,29 @@
 
 extern char **environ;
 
+/**
+ * struct Builtins - match input command to possible builtins
+ *
+ * @command: potensial builtin;
+ * @function: builtin function to execute if matched
+ *
+ * Description: The input value is split into array of pointers containing command and it's arguments. The command is then compared to command to see if it's a potensial builtin, if true the corresponding function will be executed
+ */
+
+typedef struct Builtins
+{
+	char *command;
+	int (*function)(char **);
+} Builtins;
+
+extern Builtins builtin[3];
+
 void prompt();
-char *get_input();
-int execute(char *input, char **av, size_t line_nr);
+char **get_input();
+int execute(char **input, char **av, size_t line_nr);
 char *getpath(char *name);
 char **construct_list(char *input, char *delim);
+void check_builtin(char **list);
 
 int _strlen(const char *string);
 char *_strcpy(char *dest, const char *src);
@@ -24,6 +42,8 @@ char *_strdup(const char *src);
 char *_itoa(size_t num);
 char *_strcat(char *dest, char *src);
 int _strncmp(const char *s1, const char *s2, size_t n);
+int _atoi(const char *str);
+int _strcmp(const char *s1, const char *s2);
 
 int _print(char *string);
 void error(char **av, char **list, size_t line_nr);
